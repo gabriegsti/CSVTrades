079d997278019916d4306eff3364e27565e8c872
gabriegsti <gabrielgsdevbr@gmail.com>
2022-03-15 22:52:16 -0300

    Calculo de saldos finais em andamento

--- CSVTrades/Program.cs
+++ CSVTrades/Program.cs
@@ -8,8 +8,13 @@ namespace CSVTrades
 {
     public class Program
     {
-        public static string aluno = "aluno 03";
-        public static string csv = string.Format($@"C:\Users\{aluno}\Downloads\transferoacademy_transações2.csv");
+        //
+        //public static string aluno = "aluno 03";
+        //public static string csv = string.Format($@"C:\Users\{aluno}\Downloads\transferoacademy_transações2.csv");
+
+        public static string csv = string.Format(@"C:\Users\gabri\Documents\transferoacademy_transacoes.csv");
+
+
 
         static void Main(string[] args)
         {
@@ -54,6 +59,7 @@ namespace CSVTrades
 
 
             }
+            RealizaCalculo(list);
             //"conta da Binance"
             //"29/12/2021 15:44"
             //"26/11/2020 00:33"
@@ -72,6 +78,64 @@ namespace CSVTrades
 
             Console.WriteLine("Acabou");
             Console.ReadKey();
+        }
+
+        public static void RealizaCalculo(List<Transaction> list)
+        {
+            HashSet<string> moedasNaListaSemRepeticao = new HashSet<string>();
+            List<string> moedasNaLista = new List<string>();
+            List<decimal> saldoFinal = new List<decimal>();
+            foreach (var item in list)
+            {
+                if (string.IsNullOrEmpty(item.MoedaQueEntrou))
+                    moedasNaListaSemRepeticao.Add(item.MoedaQueEntrou);
+                
+                if (string.IsNullOrEmpty(item.MoedaQueSaiu))
+                    moedasNaListaSemRepeticao.Add(item.MoedaQueSaiu);
+
+                if (string.IsNullOrEmpty(item.MoedaDeTaxa))
+                    moedasNaListaSemRepeticao.Add(item.MoedaDeTaxa);
+            }
+            moedasNaLista = moedasNaListaSemRepeticao.ToList();
+            foreach (var item in list)
+            {
+                
+                switch (item.TipoDeOperacao)
+                {
+                    case "trade":
+                        if(moedasNaLista.IndexOf(item.MoedaQueEntrou) != -1) saldoFinal[moedasNaLista.IndexOf(item.MoedaQueEntrou)] += item.Entrou;
+                        if(moedasNaLista.IndexOf(item.MoedaQueSaiu) != -1) saldoFinal[moedasNaLista.IndexOf(item.MoedaQueSaiu)] -= item.Saiu;
+                        if(moedasNaLista.IndexOf(item.MoedaDeTaxa) != -1) saldoFinal[moedasNaLista.IndexOf(item.MoedaDeTaxa)] -= item.Taxa;
+                        break;
+                    case "deposit":
+                        if (moedasNaLista.IndexOf(item.MoedaQueEntrou) != -1) saldoFinal[moedasNaLista.IndexOf(item.MoedaQueEntrou)] += item.Entrou;
+                        break;
+                    case "withdraw":
+                        if (moedasNaLista.IndexOf(item.MoedaQueSaiu) != -1) saldoFinal[moedasNaLista.IndexOf(item.MoedaQueSaiu)] -= item.Saiu;
+                        if (moedasNaLista.IndexOf(item.MoedaDeTaxa) != -1) saldoFinal[moedasNaLista.IndexOf(item.MoedaDeTaxa)] -= item.Taxa;
+                        break;
+                }
+                
+            }
+            foreach(var item in moedasNaLista)
+            {
+                switch (item)
+                {
+                    case "BCH":
+                        Console.WriteLine(item + " : " + saldoFinal[moedasNaLista.IndexOf(item)]);
+                        break;
+                    case "DOGE":
+                        break;
+                    case "USDT":
+                        break;
+                    case "XLM":
+                        break;
+                    case "XRP":
+                        break;
+
+                }
+            }
+
         }
     }
 }
